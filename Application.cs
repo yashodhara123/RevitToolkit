2cb9e3092e08322fd963807f08f444f97c81451e
Yashodhara Shiradhonkar <yashodhara.shiradhonkar@gmail.com>
2026-05-15 06:20:58 +0530

    Initial commit

---
+++ Application.cs
@@ -0,0 +1,70 @@
+﻿using Autodesk.Revit.UI;
+using Nice3point.Revit.Toolkit.External;
+using Revit_ToolKit.Commands;
+using System.Configuration.Assemblies;
+using System.Reflection;
+
+namespace Revit_ToolKit
+{
+   
+    public class Application : ExternalApplication
+    {
+        private PushButton _duplicateViews;
+        string tabName = "RevitToolKit";
+        string panelName = "RevitModelCheck";
+        public override void OnStartup()
+        {
+            RibbonPanel panel = CreateCustomRibbon(Application);
+            _duplicateViews = panel.AddItem(new PushButtonData("Check_Duplicate_Views", "Check_Duplicate_Views", Assembly.GetExecutingAssembly().Location, typeof(CheckDuplicateViewsCommand).FullName)) as PushButton;
+
+        }
+
+
+        private RibbonPanel CreateCustomRibbon(UIControlledApplication app)
+        {
+            RibbonPanel _panel = null;
+            RibbonPanel _pluginmanagerpanel = null;
+            // Try to create the tab (will throw if it already exists)
+            try
+            {
+                app.CreateRibbonTab(tabName);
+            }
+            catch
+            {
+                // Tab already exists, do nothing
+            }
+
+            // Check if panel already exists under this tab
+            bool panelExists = false;
+            bool PluginManagerpanelExists = false;
+            try
+            {
+                IList<RibbonPanel> panels = app.GetRibbonPanels(tabName);
+                foreach (RibbonPanel panel in panels)
+                {
+                    if (panel.Name.Equals(panelName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        _panel = panel;
+                        panelExists = true;
+                        //break;
+                    }
+
+                   
+                }
+            }
+            catch
+            {
+                // Tab doesn't exist or some error – ignore since we handle tab creation above
+            }
+
+            // Create panel only if it doesn't exist
+            if (!panelExists)
+            {
+
+                _panel = app.CreateRibbonPanel(tabName, panelName);
+            }
+          
+            return _panel;
+        }
+    }
+}
---
+++ Commands/CheckDuplicateViewsCommand.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Nice3point.Revit.Toolkit.External;
+
+namespace Revit_ToolKit.Commands
+{
+    /// <summary>
+    ///     External command entry point
+    /// </summary>
+   
+    [Transaction(TransactionMode.Manual)]
+    public class CheckDuplicateViewsCommand : ExternalCommand
+    {
+        public override void Execute()
+        {
+        }
+    }
+}
